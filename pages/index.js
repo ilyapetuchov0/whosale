c6d94d67112971edca4fac62c53c93aba818af75
ilyapetuchov0 <ilyapetuchov0@gmail.com>
2023-12-08 19:38:16 +0300

    added autorization and aside menu

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "@/components/Nav";
+import Layout from "@/components/Layout";
+
+export default function Home() {
+    return <Layout>
+        test
+    </Layout>
+}
